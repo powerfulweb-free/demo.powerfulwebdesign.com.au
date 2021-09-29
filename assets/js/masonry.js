7e51be378ab7cd08ab786a8b5aee5028fd4e0425
sean-pwd <sean@powerfulwebdesign.com.au>
2021-09-30 02:00:59 +1000

    fix: moved scripts to header

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,11 +1,8 @@
 import Masonry from 'masonry-layout';
-
 var msnry = new Masonry('#js-masonryGrid', {
   itemSelector: '.js-masonry-item',
   percentPosition: true,
 });
 
-/* 
-
 // on bs row
- data-masonry='{"percentPosition": true, "itemSelector": ".masonry-item" }'  */
+// data-masonry='{"percentPosition": true}'  
